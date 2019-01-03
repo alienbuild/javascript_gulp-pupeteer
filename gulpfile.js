a205db220c66fd32dfd6d44b1accb4594fb8f05a
LK <32590877+alienbuild@users.noreply.github.com>
2019-01-03 08:35:19 +0000

    Added modal support
    • Now tests for the presence of modals that are triggered on elements that contain a data-toggle attribute to launch them.
    • The data-toggle element should contain text which will be applied to the filename when saving. 
    • Modals are closed by clicking in mouse position 0, 0 - You may need to change this to fit your own close location.

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,130 @@ const runSequence = require('run-sequence');
 const shotIt = async (page, fileName) => {
     return new Promise(async(resolve, reject) => {
         try{
-            await page.setUserAgent("puppeteer");
-            await page.goto(fileName);
+
+            // Create Delay Function
+            function delay(time) {
+               return new Promise(function(resolve) { 
+                   setTimeout(resolve, time)
+               });
+           }
+
+           // Set Puppeteer and Proceed to Files
+           await page.setUserAgent("puppeteer");
+           await page.goto(fileName);
+
+           const bodyHandle = await page.$('body');
+
+            // Set Desktop Viewport
             await page.setViewport({
-              width: 1920,
-              height: 1080
-          });
-            const bodyHandle = await page.$('body');
-            const bbb = Date.now();
-            console.log("Taking screenshot of: " + fileName);
-            await page.screenshot({
-                fullPage: true,
-                path: fileName.split(".html")[0]+".jpg"
+                width: 1920,
+                height: 1080
             });
-            await bodyHandle.dispose();
-            resolve();
-        }catch(err){
-            reject(err);
-        };
-    });
+
+            // If a button with data-toggle attr exists
+            if ((await page.$('[data-toggle]')) !== null) {
+                console.log('----- Modals Found -----');
+
+                await delay(1000);
+
+                // Grab all buttons
+                const items = await page.$$('[data-toggle]');
+
+                // For each button perform tasks
+                for (let i = 0, length = items.length; i < length; i++) {
+
+                    const item = await page.evaluateHandle((i) => {
+                        const element = document.querySelectorAll('[data-toggle]')[i];
+                        return element;
+                    }, i);
+
+                    // Click selector
+                    await item.click();
+
+                    // Get button text
+                    let itemText = await (await item.getProperty('textContent')).jsonValue();
+                    const modalName = itemText.trim();
+                    const modalFileName = modalName.replace(/\s+/g, '-').toLowerCase();
+
+                    // Wait for modal to slide in
+                    await delay(2000);
+                    
+                    // Take Desktop Screenshot
+                    console.log('[Modal #'+i+'] [' +modalName+'] [Desktop] Taking screenshot...');
+                    await page.screenshot({
+                        fullPage: true,
+                        path: fileName+'-'+modalFileName+'_modal_desktop'.split(".html")[0]+".jpg"
+                    });
+
+                    // Set Mobile Viewport
+                    await page.setViewport({
+                        width: 375,
+                        height: 812
+                    });
+
+                    // Take Mobile Screenshot
+                    console.log('[Modal #'+i+'] [' +modalName+'] [Mobile] Taking screenshot...');
+                    await page.screenshot({
+                        fullPage: true,
+                        path: fileName+'-'+modalFileName+'_modal_mobile'.split(".html")[0]+".jpg"
+                    });
+
+                    // Set Desktop Viewport
+                    await page.setViewport({
+                        width: 1920,
+                        height: 1080
+                    });
+
+                    // Close Modal
+                    await page.mouse.click(0, 0); // Work around as click element doesn't work after a few times.
+                    console.log('---------');
+
+                    // Wait for modal to slide out
+                    await delay(2000);
+
+                }
+
+                // Take screenshot
+                console.log('Taking screenshot of:', fileName);
+                await page.screenshot({
+                    fullPage: true,
+                    path: fileName+'_desktop'.split(".html")[0]+".jpg"
+                });
+
+                } else{
+
+                    console.log("No modals exists, taking desktop screenshot of: " + fileName);
+
+                    // Set Desktop Viewport
+                    await page.setViewport({
+                        width: 1920,
+                        height: 1080
+                    });
+
+                    // Take screenshot
+                    await page.screenshot({
+                        fullPage: true,
+                        path: fileName+'_desktop'.split(".html")[0]+".jpg"
+                    });
+
+                    // Set Mobile Viewport
+                    await page.setViewport({
+                        width: 375,
+                        height: 812
+                    });
+
+                     await page.screenshot({
+                        fullPage: true,
+                        path: fileName+'_mobile'.split(".html")[0]+".jpg"
+                    });
+                }
+
+                await bodyHandle.dispose();
+                resolve();
+            }catch(err){
+                reject(err);
+            };
+        });
 };
 
 const screenshots = () => {
